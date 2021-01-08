87ea13752aac1e9763b85bdacb190b0a05a0831f
Melika Emma Vigh Kjærgaard Pedersen <meli0970@edu.mercantec.dk>
2021-01-08 08:13:51 +0100

    Add project files.

---
+++ Game/Game/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Game
+{
+    public partial class MainPage : ContentPage
+    {
+        static string val = new Random().Next(1, 100000).ToString();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            var checkVal = guess.Text;
+            if (checkVal == val)
+            {
+                await DisplayAlert("Guess that number", "!!You guessed correctly!!", "Try again");
+                val = new Random().Next(1, 100000).ToString();
+            }
+            else if (int.Parse(checkVal) < int.Parse(val))
+            {
+                await DisplayAlert("Guess that number", "You guessed too low", "Try again");
+            }
+            else if (int.Parse(checkVal) > int.Parse(val))
+            {
+                await DisplayAlert("Guess that number", "You guessed too high", "Try again");
+            }
+            else
+            {
+                await DisplayAlert("Guess that number", "Only use numbers!", "Try again");
+            }
+        }
+    }
+}
